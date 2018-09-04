da6b802b15cbe8fd0c27d3978d61edd2622bdd2e
marcinzak77 <marcin.zak77@gmail.com>
2018-09-04 08:49:33 +0200

    db fix 2

--- src/main/java/com/library/controller/ItemController.java
+++ src/main/java/com/library/controller/ItemController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/library")
 @CrossOrigin(origins = "*")
 public class ItemController {
     private final ItemService itemService;
 
-    @RequestMapping(method = RequestMethod.POST, value = "addItem")
+    @PostMapping(value = "addItem")
     public void addBookCopy(@RequestParam int titleId) {
         itemService.addItem(titleId);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteItem")
+    @DeleteMapping(value = "deleteItem")
     public void removeBookCopy(@RequestParam int bookId) {
         itemService.deleteItem(bookId);
     }

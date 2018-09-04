63547ce04bbf827c664dd1faf6eef51141e8dc70
marcinzak77 <marcin.zak77@gmail.com>
2018-09-04 14:27:43 +0200

    json fix

--- src/main/java/com/library/domain/dto/BookDto.java
+++ src/main/java/com/library/domain/dto/BookDto.java
@@ -1,5 +1,6 @@
 package com.library.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,11 @@ import java.util.List;
 @AllArgsConstructor
 public class BookDto {
     private int titleId;
+    @JsonProperty("author")
     private String author;
+    @JsonProperty("title")
     private String title;
+    @JsonProperty("releaseDate")
     private int releaseDate;
     private List<ItemDto> items;
 }

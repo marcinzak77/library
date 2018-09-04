8fee8dc8064432c4034fc6a91656d340dda58aee
marcinzak77 <marcin.zak77@gmail.com>
2018-09-04 09:03:23 +0200

    db fix

--- src/main/java/com/library/controller/BookController.java
+++ src/main/java/com/library/controller/BookController.java
@@ -17,22 +17,22 @@ public class BookController {
     private final BookService bookService;
     private final LibraryMapper libraryMapper;
 
-    @RequestMapping(method = RequestMethod.POST, value = "addBook")
+    @PostMapping(value = "addBook")
     public void addBook(@RequestBody BookDto bookDto) {
         bookService.addBook(libraryMapper.mapToBook(bookDto));
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteBook")
+    @DeleteMapping(value = "deleteBook")
     public void deleteBook(@RequestParam int titleId) {
         bookService.deleteBook(titleId);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getBooks")
+    @GetMapping(value = "getBooks")
     public List<Book> getAllBooks() {
         return bookService.getAllBooks();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "findBookTitle")
+    @GetMapping(value = "findBookTitle")
     public BookDto findBookByTitle(@RequestParam String bookTitle) {
         return libraryMapper.mapToBookDto(bookService.findBook(bookTitle));
     }
